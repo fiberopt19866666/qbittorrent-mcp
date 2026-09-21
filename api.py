76a0c537d3f1178db241db3a84dbf3078527d50e
Darren Meunier <fiberopticalrefraction@gmail.com>
2026-09-21 13:03:56 -0400

    Fixed Cloudflare environment variabla names

--- api.py
+++ api.py
@@ -18,8 +18,8 @@ SEARCH_RETRY_DELAY_SECONDS = 1.0
 def cf_access_headers() -> Dict[str, str]:
     """Return Cloudflare Access headers from env vars, if configured."""
     headers = {}
-    client_id = os.getenv("CF_CLIENT_ID")
-    client_secret = os.getenv("CF_CLIENT_SECRET")
+    client_id = os.getenv("CF_ACCESS_CLIENT_ID")
+    client_secret = os.getenv("CF_ACCES_CLIENT_SECRET")
     if client_id:
         headers["CF-Access-Client-Id"] = client_id
     if client_secret:
