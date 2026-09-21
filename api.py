e27137a26133a8fe3a0a6dfa9d8f94239811ab80
fiberopt19866666 <fiberopticalrefraction@gmail.com>
2026-09-21 11:23:48 -0400

    added debug / debugging

--- api.py
+++ api.py
@@ -44,7 +44,9 @@ async def login_to_qbittorrent(username, password, host):
             f"{host}/api/v2/auth/login",
             data={"username": username, "password": password},
         )
-
+        print(f"Content: {response.json()}", file=sys.stderr)
+        print(f"URL: {response.url}")
+        print(f"Text: {response.text}")
         # qBittorrent v5.x 返回 204，v4.x 返回 200
         if response.status_code in (200, 204):
             return response.cookies
